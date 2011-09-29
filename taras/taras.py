6158a721aaab8befd4ca53511ab59127bd212176
luanjunyi <luanjunyi@c8f40f87-74ee-d30c-42bf-5e0dbc18c800>
2011-09-29 08:47:23 +0000

    update

--- taras/taras.py
+++ taras/taras.py
@@ -1200,7 +1200,6 @@ class WeiboDaemon:
                             self.freeze_user(user)
                         except Exception, err:
                             _logger.error('get_api_by_user failed, but not WeibopError: %s', err)
-                            print traceback.format_exc()
                         else:
                             _logger.debug("api generated for user(%s)" % self.user.uname)
 
