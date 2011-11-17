d4148bf6a81a12532447ea9b5483993814f4cd61
luanjunyi <luanjunyi@c8f40f87-74ee-d30c-42bf-5e0dbc18c800>
2011-11-17 08:28:34 +0000

    update

--- third_party/weibopy/error.py
+++ third_party/weibopy/error.py
@@ -6,7 +6,8 @@ class WeibopError(Exception):
     """Weibopy exception"""
 
     def __init__(self, reason):
-        self.reason = reason.encode('utf-8')
+        if hasattr(reason, 'encode'):
+            self.reason = reason.encode('utf-8')
 
     def __str__(self):
         return self.reason
