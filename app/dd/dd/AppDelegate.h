3f51bb67da89f6debf6d8ffc518c5b8812d75062
luanjunyi <luanjunyi@c8f40f87-74ee-d30c-42bf-5e0dbc18c800>
2012-03-27 09:41:36 +0000

    update

---
+++ app/dd/dd/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  dd
+//
+//  Created by luanjunyi on 3/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
