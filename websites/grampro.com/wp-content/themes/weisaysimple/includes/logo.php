5dbd931719e91ef7b732e62db715b50e19825b75
luanjunyi <luanjunyi@c8f40f87-74ee-d30c-42bf-5e0dbc18c800>
2013-05-28 12:07:45 +0000

    remove websites

--- websites/grampro.com/wp-content/themes/weisaysimple/includes/logo.php
+++
@@ -1,2 +0,0 @@
-	<div id="blogname"><a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a>
-    <div id="blogtitle"><?php bloginfo('description'); ?></div></div>
--- websites/grampro.com/wp-content/themes/weisaysimple/includes/r_tags.php
+++
@@ -1,2 +0,0 @@
-<h3>标签云集</h3>	
-<?php wp_tag_cloud('smallest=12&largest=16&unit=px&number=65&orderby=count&order=RAND');?>
